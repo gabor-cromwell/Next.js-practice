62b74426907020f73505c4ca0b419ac298911dc9
Havasi <gabor.havasi@cromwell.co.uk>
2022-11-22 12:13:24 +0000

    Static routing

---
+++ routing/pages/blog/first.js
@@ -0,0 +1,9 @@
+const FirstBlog = () => {
+  return (
+    <div>
+      <h2>First blog page</h2>
+    </div>
+  );
+};
+
+export default FirstBlog;
---
+++ routing/pages/blog/second.js
@@ -0,0 +1,9 @@
+const SecondBlog = () => {
+  return (
+    <div>
+      <h2>Second blog page</h2>
+    </div>
+  );
+};
+
+export default SecondBlog;
