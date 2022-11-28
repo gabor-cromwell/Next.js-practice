1db70e35bd18f4769e892e1c7b0c0832ddc62e8f
Havasi <gabor.havasi@cromwell.co.uk>
2022-11-28 16:02:52 +0000

    SWR hook practice

--- next-hook/pages/photos/index.js
+++ next-hook/pages/photos/index.js
@@ -1,5 +1,34 @@
+import useSWR from "swr";
+
+const fetcher = async () => {
+  const response = await fetch("https://jsonplaceholder.typicode.com/photos");
+  const data = response.json();
+  return data;
+};
+
 const Photos = () => {
-  return <div>This is the photo page</div>;
+  const { data, error } = useSWR("photos", fetcher);
+
+  if (error) {
+    return "There was an error";
+  }
+
+  if (!data) {
+    return "Loading....";
+  }
+
+  return (
+    <div>
+      <h3>This is the photo page</h3>
+      {data.map((photo) => {
+        return (
+          <p key={photo.id}>
+            {photo.id} - {photo.title} - {photo.url}
+          </p>
+        );
+      })}
+    </div>
+  );
 };
 
 export default Photos;
